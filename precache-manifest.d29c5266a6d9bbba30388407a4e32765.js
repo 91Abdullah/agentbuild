c71c01bef1e571e0c38e300578f9b37c67ef250d
Abdullah Basit <abdullah.basit@hotmail.com>
2020-11-27 15:05:27 +0500

    Build for agent

---
+++ precache-manifest.d29c5266a6d9bbba30388407a4e32765.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54d649bbc722ec5980d995d47d7132b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba4bf623a5f30e674602",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "3bea6fa17dfe72419788",
+    "url": "/static/css/main.f610c72e.chunk.css"
+  },
+  {
+    "revision": "ba4bf623a5f30e674602",
+    "url": "/static/js/2.8214ce58.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8214ce58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bea6fa17dfe72419788",
+    "url": "/static/js/main.ee388228.chunk.js"
+  },
+  {
+    "revision": "1748f8291bf52cb2ead8",
+    "url": "/static/js/runtime-main.79094af0.js"
+  },
+  {
+    "revision": "96f7c6f1779bddfc89b93ea7877e62ce",
+    "url": "/static/media/0.96f7c6f1.wav"
+  },
+  {
+    "revision": "5cf316051736983bd012b2d4142806b9",
+    "url": "/static/media/1.5cf31605.wav"
+  },
+  {
+    "revision": "e59b907216fc013071d3347d295af40a",
+    "url": "/static/media/2.e59b9072.wav"
+  },
+  {
+    "revision": "edc2da23871ec62763a20ea4717e7278",
+    "url": "/static/media/3.edc2da23.wav"
+  },
+  {
+    "revision": "ae1de26d3469c09e25cadb356bda00ef",
+    "url": "/static/media/4.ae1de26d.wav"
+  },
+  {
+    "revision": "1cdb9034806aabc1577027b49a6bb672",
+    "url": "/static/media/5.1cdb9034.wav"
+  },
+  {
+    "revision": "9ba7805a002d96fbde7a25eff8eb9275",
+    "url": "/static/media/6.9ba7805a.wav"
+  },
+  {
+    "revision": "ce7c77b6487a3399de3c13c89612567f",
+    "url": "/static/media/7.ce7c77b6.wav"
+  },
+  {
+    "revision": "281df24a7373466707f274991d6ffc12",
+    "url": "/static/media/8.281df24a.wav"
+  },
+  {
+    "revision": "f25bbf24175666f104c604b905124b38",
+    "url": "/static/media/9.f25bbf24.wav"
+  },
+  {
+    "revision": "248be4b3a043ed4aff0cd453863e55e0",
+    "url": "/static/media/p.248be4b3.wav"
+  },
+  {
+    "revision": "ac6f9d10ef693096a16c6c4efa4b7550",
+    "url": "/static/media/ring.ac6f9d10.mp3"
+  },
+  {
+    "revision": "1b621124105ad228bf9c8c142f3593b2",
+    "url": "/static/media/star.1b621124.wav"
+  }
+]);
